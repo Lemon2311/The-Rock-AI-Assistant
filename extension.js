3f2a3f354c02154b7eee3e5a1a86b7ea907528cd
Lemon2311 <marcotighiliu@yahoo.com>
2023-12-20 14:16:52 +0100

    working code placed in functions, working on core functionality

--- extension.js
+++ extension.js
@@ -1,5 +1,4 @@
 const vscode = require('vscode');
-const path = require('path');
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -7,43 +6,18 @@ const path = require('path');
 function activate(context) {
     console.log('Congratulations, your extension "the-rock-ai-assistant" is now active!');
 
-    // Register the 'helloWorld' command
+    registerHelloWorldCommand(context);
+    registerInputCommand(context);
+}
+
+function registerHelloWorldCommand(context) {
     let disposableHelloWorld = vscode.commands.registerCommand('the-rock-ai-assistant.helloWorld', () => {
         vscode.window.showInformationMessage('Hello World from The Rock AI Assistant!');
     });
     context.subscriptions.push(disposableHelloWorld);
+}
 
-    // Create a simple tree data provider
-    class IconProvider {
-        getTreeItem(element) {
-            return element;
-        }
-
-        getChildren() {
-            // Creating a TreeItem with an icon
-            const iconPath = path.join(__filename, '..', '..', 'media', 'icon.svg');
-            const treeItem = new vscode.TreeItem("Icon Item");
-            treeItem.iconPath = {
-                light: vscode.Uri.file(iconPath),
-                dark: vscode.Uri.file(iconPath)
-            };
-            return [treeItem];
-        }
-    }
-
-    // Register the TreeDataProvider
-    const iconProvider = new IconProvider();
-    vscode.window.registerTreeDataProvider('iconView', iconProvider);
-
-    // Create a status bar item
-    let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-    statusBarItem.command = 'the-rock-ai-assistant.inputCommand';
-    statusBarItem.text = "$(keyboard) Add Input";
-    statusBarItem.tooltip = "Click to add input";
-    statusBarItem.show();
-    context.subscriptions.push(statusBarItem);
-
-    // Register the 'inputCommand' command
+function registerInputCommand(context) {
     let disposableInputCommand = vscode.commands.registerCommand('the-rock-ai-assistant.inputCommand', () => {
         vscode.window.showInputBox({ placeHolder: "Enter your input here" })
             .then(value => {
